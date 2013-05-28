3951b27a1a47cf60a1165ed1a179bbb35909c170
xgam33 <xgam33@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-28 11:01:44 +0000

    part of XSDParser

--- WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/XSDParser.java
+++ WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/XSDParser.java
@@ -1,13 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.cz.muni.fi.pb138.webrep_A;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
-
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 /**
  *
  * @author xfasian
@@ -17,8 +19,33 @@ public class XSDParser {
     private List<String> simpleTypes;
     private List<String> complexTypes;
     
-    public Document XSDExtract(Document doc){
-        return null;
+    public Document XSDExtract(Document doc) throws ParserConfigurationException{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        int i;
+        Document output;
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        output = builder.newDocument(); 
+
+        NodeList complexTypeList = doc.getElementsByTagName("complexType");
+        NodeList simpleTypeList = doc.getElementsByTagName("simpleType");
+
+        Element root = (Element)output.createElement("types"); 
+        output.appendChild(root);
+        for (i = 0; i < complexTypeList.getLength(); i++) {
+            if (complexTypeList.item(i) instanceof Element) {
+                Element complexElement = (Element) complexTypeList.item(i);
+                Node newcom = doc.createElement("complexType");
+                root.appendChild(newcom);
+            }
+        }
+        for (i = 0; i < simpleTypeList.getLength(); i++) {
+            if (simpleTypeList.item(i) instanceof Element) {
+                Element simpleElement = (Element) simpleTypeList.item(i);
+                Node newsim = doc.createElement("simpleType");
+                root.appendChild(newsim);
+            }
+        }
+        return output;
     }
     
     private Map<String,List<String>> getElements(Document doc){
@@ -26,7 +53,18 @@ public class XSDParser {
     }
     
     private List<String> getSimpleTypes(Document doc){
-        return null;
+        int i;
+        List<String> simpleTypesOut = new ArrayList<String>();
+        NodeList simpleTypeList = doc.getElementsByTagName("simpleType");
+        for (i = 0; i < simpleTypeList.getLength(); i++) {
+            if (simpleTypeList.item(i) instanceof Element) {
+                Element simpleElement = (Element) simpleTypeList.item(i);
+                Node newsim = doc.createElement("simpleType");
+                //simpleTypesOut.add(newsim);
+                //to be continued? or deleted
+            }
+        }
+        return simpleTypesOut;
     }
     
     private List<String> getComplexTypes(Document doc){
