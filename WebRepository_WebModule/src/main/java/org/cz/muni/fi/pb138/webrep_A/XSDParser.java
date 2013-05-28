2a36e5d36e565fe613d03d871d420647875b4a0c
xgam33 <xgam33@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-28 11:21:00 +0000

    XSDParser update

--- WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/XSDParser.java
+++ WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/XSDParser.java
@@ -22,13 +22,17 @@ public class XSDParser {
     public Document XSDExtract(Document doc) throws ParserConfigurationException{
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         int i;
+        String namespace = "xs:";
+        //doimplementovat get z root elementu cist dokud ":"
         Document output;
         DocumentBuilder builder = factory.newDocumentBuilder();
         output = builder.newDocument(); 
 
-        NodeList complexTypeList = doc.getElementsByTagName("complexType");
-        NodeList simpleTypeList = doc.getElementsByTagName("simpleType");
-
+        NodeList complexTypeList = doc.getElementsByTagName(namespace+"complexType");
+        NodeList simpleTypeList = doc.getElementsByTagName(namespace+"simpleType");
+        NodeList elementsList = doc.getElementsByTagName(namespace+"element");
+        NodeList attributeList = doc.getElementsByTagName(namespace+"attribute");
+        
         Element root = (Element)output.createElement("types"); 
         output.appendChild(root);
         for (i = 0; i < complexTypeList.getLength(); i++) {
@@ -43,6 +47,20 @@ public class XSDParser {
                 Element simpleElement = (Element) simpleTypeList.item(i);
                 Node newsim = doc.createElement("simpleType");
                 root.appendChild(newsim);
+            }
+        }
+        for (i = 0; i < elementsList.getLength(); i++) {
+            if (elementsList.item(i) instanceof Element) {
+                Element elementElement = (Element) elementsList.item(i);
+                Node newele = doc.createElement("element");
+                root.appendChild(newele);
+            }
+        }
+        for (i = 0; i < attributeList.getLength(); i++) {
+            if (attributeList.item(i) instanceof Element) {
+                Element attElement = (Element) attributeList.item(i);
+                Node newatt = doc.createElement("attribute");
+                root.appendChild(newatt);
             }
         }
         return output;
