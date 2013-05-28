9fbb73b2149b8fbca4a37c53257020aeedb1cdb1
xgam33 <xgam33@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-28 16:47:09 +0000

    War Parser

--- WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WarArchiveParser.java
+++ WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WarArchiveParser.java
@@ -1,30 +1,46 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.cz.muni.fi.pb138.webrep_A;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author xfasian
  */
 public class WarArchiveParser { 
-    private List<String> filtres;
-    private List<String> listeners;
-    
-    public Document WARExtract(Document doc){
-        return null;
-    }
-    
-    private List<String> getFilters(Document doc){
-        return null;
-    }
-    private List<String> getListeners(Document doc){
-        return null;
-    }
-    
 
+    public Document WARExtract(JarFile warFile) throws ParserConfigurationException, SAXException, IOException{
+        Enumeration entries = warFile.entries();
+        while (entries.hasMoreElements()) {
+           ZipEntry entry = (ZipEntry)entries.nextElement();
+           if (entry.getName().equals("web.xml")) {
+                break;
+           }
+           InputStream in = null;
+           in = warFile.getInputStream(entry);
+           
+           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+           DocumentBuilder builder = factory.newDocumentBuilder();
+           Document output = null;
+            try {
+                output = builder.parse(in);
+            } catch (SAXException ex) {
+                throw new SAXException(ex);
+            } catch (IOException ex) {
+                throw new IOException(ex);
+            }
+           return output;
+        }
+        throw new IllegalArgumentException("no web.xml in .war file");
+
+    }
 }
