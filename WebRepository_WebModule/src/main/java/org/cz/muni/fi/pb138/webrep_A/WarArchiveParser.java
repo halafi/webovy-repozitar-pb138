0e794c2cb07414d7a73dc80b526eb76c212b06d6
xgam33 <xgam33@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-29 15:50:28 +0000

    wsdl war

--- WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WarArchiveParser.java
+++ WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WarArchiveParser.java
@@ -1,6 +1,4 @@
-
 package org.cz.muni.fi.pb138.webrep_A;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
@@ -20,27 +18,25 @@ public class WarArchiveParser {
 
     public Document WARExtract(JarFile warFile) throws ParserConfigurationException, SAXException, IOException{
         Enumeration entries = warFile.entries();
+        InputStream in = null;
         while (entries.hasMoreElements()) {
            ZipEntry entry = (ZipEntry)entries.nextElement();
            if (entry.getName().equals("web.xml")) {
-                break;
+               in = warFile.getInputStream(entry);
+               break;
+               //prohledava podslozky?
            }
-           InputStream in = null;
-           in = warFile.getInputStream(entry);
-           
-           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-           DocumentBuilder builder = factory.newDocumentBuilder();
-           Document output = null;
-            try {
-                output = builder.parse(in);
-            } catch (SAXException ex) {
-                throw new SAXException(ex);
-            } catch (IOException ex) {
-                throw new IOException(ex);
-            }
-           return output;
         }
-        throw new IllegalArgumentException("no web.xml in .war file");
-
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document output = null;
+         try {
+             output = builder.parse(in);
+         } catch (SAXException ex) {
+             throw new SAXException(ex);
+         } catch (IOException ex) {
+             throw new IOException(ex);
+         }
+        return output;
     }
 }
