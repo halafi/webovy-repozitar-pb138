ca0599f60fa5525d945874dad379cda8e9fd4e80
xgam33 <xgam33@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-29 08:23:47 +0000

    XSDParser update

--- trunk/WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WSDLDocManagerImpl.java
+++ trunk/WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/WSDLDocManagerImpl.java
@@ -1,17 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.cz.muni.fi.pb138.webrep_A;
 
 import java.io.File;
-import java.text.MessageFormat;
 import java.util.List;
 import org.basex.core.BaseXException;
-import org.basex.core.Context;
+import org.basex.core.cmd.Close;
 import org.basex.core.cmd.CreateDB;
 import org.basex.core.cmd.Open;
 import org.basex.core.cmd.XQuery;
+import org.basex.query.expr.Context;
 import org.w3c.dom.Document;
 /**
  *
@@ -23,87 +19,101 @@ public class WSDLDocManagerImpl implements WSDLDocManager {
     private static final String XML_HEAD = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     private static final String OPENING_TAG = "<webxmls>";
     private static final String CLOSING_TAG = "</webxmls>";
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(WSDLDocManagerImpl.class);
     
-    public WSDLDocManagerImpl() throws Exception
+    public WSDLDocManagerImpl()
     {
-        dbCtx = new Context();
-        try 
+        dbCtx = new Context() {};
+        try
         {
             File yourFile = new File("src/main/resources/xml/wsdlDatab.xml");
             if(!yourFile.exists()) {
             new CreateDB("WSDLDatab", "src/main/resources/xml/wsdlDatab.xml").execute(dbCtx);
+	    new Close.execute(dbCtx);
             }
-            openDB();
-        } 
-        catch (BaseXException ex) 
+           
+        }
+        catch (BaseXException ex)
         {
-            throw new Exception("Database could not be opened or created", ex);
-       }
+            logger.log(ex);
+       } 
     }
-    
-     private void openDB() throws BaseXException
+     
+     /*private void openDB() throws BaseXException
     {
         new Open("src/main/resources/xml/wsdlDatab.xml").execute(dbCtx);
     }
-    
-    
-    
-    
+     */
+     
+     
+     
     @Override
-    public void createWSDL(WSDLDoc wsdl) throws Exception {
+    public void createWSDL(WSDLDoc wsdl){
         if (wsdl.getId() != null) {
-            throw new IllegalArgumentException("wsdl id is already set");            
+            throw new IllegalArgumentException("wsdl id is already set");           
         }
             String insertQuery = "insert node {0} into {1}";
         try
         {
-            new XQuery(MessageFormat.format(insertQuery, wsdl.getDocument(), "/wsdl")).execute(dbCtx);
+		// test query for input
+	    new Open(wsdlDatab.xml).execute(dbCtx);
+	    new XQuery(
+        "declare namespace xhtml='http://www.w3.org/1999/xhtml';" +
+        "insert node " +
+        " <xhtml:p>I will match the following query because I contain" +
+        " the terms 'ARTICLE' and 'EDITABLE'. :-)</xhtml:p> " +
+        "into //xhtml:body"
+         ).execute(context);
+
+            //new XQuery(MessageFormat.format(insertQuery, wsdl.getDocument(), "/wsdl")).execute(dbCtx);
         }
         catch (BaseXException ex)
         {
-           throw new Exception("error while inserting data into database", ex);
-        }
-        
+           logger.log(ex);
+        } finally {
+	
+	new Close(wsdlDatab.xml).execute(dbCtx);
+}
+         
     }
-    
+     
     @Override
     public WSDLDoc getWSDL(Integer id) {
         if (id == null) {
             throw new IllegalArgumentException("id is null");
         }
         /*try {
-            
+             
         }
         catch {
-            
+             
         }
         finally {
           close db, etc.
         }*/
         return null;
     }
-    
+     
     @Override
     public List<WSDLDoc> getAllWSDLs() {
-        
+         
         /*try {
-            
+             
         }
         catch {
-            
+             
         }
         finally {
           close db, etc.
         }*/
         return null;
     }
-    
+     
     /*
      * Finds WSDL by metadata.
      */
     @Override
     public List<WSDLDoc> findWSDLByData(Document extract){
-        
         return null;
     }
 }
