1ce8de5c287952d131b24979ed165fdf6e73e2e3
peterfasianok@gmail.com <peterfasianok@gmail.com@f9507551-a58f-932f-ed86-1def710d5e61>
2013-05-30 13:53:51 +0000

    webxmlParser

--- trunk/WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/Parser/WebXMLParser.java
+++ trunk/WebRepository_WebModule/src/main/java/org/cz/muni/fi/pb138/webrep_A/Parser/WebXMLParser.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
@@ -18,31 +19,28 @@ import org.w3c.dom.NodeList;
 public class WebXMLParser {
     public Document webXMLExtract(Document doc) throws ParserConfigurationException{
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        int i;
-                
         Document output;
         DocumentBuilder builder = factory.newDocumentBuilder();
         output = builder.newDocument(); 
 
-        NodeList operationList = doc.getElementsByTagName("listener");
-        NodeList messageList = doc.getElementsByTagName("filter");
-        
-        Element root = (Element)output.createElement("filters_and_listeners"); 
+        Element root = (Element) output.createElement("filters_and_listeners"); 
         output.appendChild(root);
-        for (i = 0; i < operationList.getLength(); i++) {
-            if (operationList.item(i) instanceof Element) {
-                Element listenerElement = (Element) operationList.item(i);
-                //Node newListener = doc.createElement("listener");
-                //root.appendChild(newListener);
-                root.appendChild(listenerElement);
+        
+        NodeList filterList = doc.getElementsByTagName("filter");
+        for (int i = 0; i < filterList.getLength(); i++) {
+            if (filterList.item(i) instanceof Element) {
+                Element operationElement = (Element) filterList.item(i);
+                Node nodeToMove = output.importNode(operationElement, true);
+                root.appendChild(nodeToMove);
             }
         }
-        for (i = 0; i <  messageList.getLength(); i++) {
-            if (messageList.item(i) instanceof Element) {
-                Element filterElement = (Element) messageList.item(i);
-                //Node newMessage = doc.createElement("filter");
-                //root.appendChild(newMessage);
-                root.appendChild(filterElement);
+        
+        NodeList listenerList = doc.getElementsByTagName("listener");        
+        for (int i = 0; i <  listenerList.getLength(); i++) {
+            if (listenerList.item(i) instanceof Element) {
+                Element messageElement = (Element) listenerList.item(i);
+                Node nodeToMove = output.importNode(messageElement, true);
+                root.appendChild(nodeToMove);
             }
         }
         return output;
